2a75b755593209f20f600a1ae0ce8e925ec1e522
SovietToast <32574665+SovietToast@users.noreply.github.com>
2017-12-13 08:57:38 -0500

    Add files via upload
    This is a bit of thr progect skeiton

---
+++ ske.py
@@ -0,0 +1,188 @@
+#Sean Towse 12.4.17
+import time
+import string
+def main():
+    dictonryGenral = {player:}
+    getPlayerName()
+    greetUser()
+    createCharicter(userName) 
+    while createdCharcterAlive == true:
+        getWalkDirection(userName)
+        if room == fight:
+            gererateEnameyWeighted(userSkills, userEqu)
+            #store stats
+               comabat(userSkills, userEqu, botSkills, botEqu)
+                  if playerWon == yes:
+                      #cngadulations
+                      #REMOVE ENAMY FROM Dictory
+                  if playerWon == no
+                      gameover(userName)
+               
+               
+        if room == loot:
+       
+            
+        
+     
+def comabat(dictonryGenral):
+          userThrow = getPlayerThrow()
+          botThrow = getBotThrow()
+          speedCaculation()
+          if userThrow == first:
+           caculations(userThrow)
+           #update dicotnary
+           if damage == dehablitating: 
+               #skip turn for second
+           if damage == true:
+               #apply debuff
+           caculations(botThrow)
+           #calcuations baised of type of strike
+          if botThrow == first:
+              caculations(botthrow)
+              #update dicotnary
+              if damage == dehablitating: 
+                  #skip turn for second
+              if damage == true:
+                  #apply debuff
+              caculations(userThrow)
+          #do the next one
+          #announce results of the round
+            #update the player status
+          #next tick
+          #if player == dead
+              #announce final results
+              #say the game is over
+              
+          return Winner
+def getUserName():
+    #code to get the user's name goes here
+
+
+    #this is the return statement that will send the value back
+    return userName
+
+
+
+def greetUser(userName):
+    #code to greet the user goes here
+
+
+    #no return statement because this function doesn't answer a question; it just does a job
+def createCharicter(userName, dictonryGenral):
+    #skillDictBlank = {Player:  'Int': ,'Chr' : ,'Str': ,'Con' : ,'Wis' : ,'Dex': }
+    #for skill dic assiagn valuse by asking user 
+    #ablitysDict = {'S
+    #ablity
+    #1-5 very bad
+    #5-11 normal
+    #12-15 - Above Average. 
+    #16-19 - Genius-level.  savants, "gifted,"
+    #20-23 - Superhuman. 
+    #24+ - crqazy good
+
+    #run a loop about chooseing each of these 
+        #hand to an asking function?
+    return #add results to dictonary
+def gererateEnameyWeighted(userSkills, userEqu):
+    #skillDictBlank = {Player:  'Int': ,'Chr' : ,'Str': ,'Con' : ,'Wis' : ,'Dex': }
+    #for skill dic assiagn valuse by asking user 
+    #ablitysDict = {'S
+    #ablity
+    #1-5 very bad
+    #5-11 normal
+    #12-15 - Above Average. 
+    #16-19 - Genius-level.  savants, "gifted,"
+    #20-23 - Superhuman. 
+    #24+ - crqazy good
+
+    #run a loop about chooseing each of these 
+        #hand to an asking function?
+    return #add results to dictonary
+witchWayToWalk():
+   promt = "witch way would you like to walk? North, South, East, or West"
+   userInput = raw_input promt
+   userInput = string.lower promt
+   if north or south or east or west in userInput:
+       randEncouter()
+def randEncouter():
+    #randdom gereration of stuff
+def getUserThrow(throwList):
+    #code to get the user's throw goes here
+   
+
+    #this is the return statement that will send the value back
+    return userThrow
+
+
+def getComputerThrow(throwList):
+#code to get the computer's throw goes here
+
+
+    #this is the return statement that will send the value back
+    return computerThrow
+def whoHitsFirst(userthrow, userEqu,botThrow, botEqu):
+    #take the speed of bolth, find who hits first and retun boolth
+        #
+    return firstToHit & secondToHit 
+def caculations(strikeFirstToHit):
+    #if blocking
+    #if stabbing/bashing
+        #area of point of weapon / speed = 
+        #compare to armor
+        #check siccues 
+    #if slashing 
+        #find points that hit 
+        #caculate thier speed using the radius of the thing 
+        # comapre to arourmor 
+            #change to blunt if not breaking layer
+        #if slashing dmg higher than armour deformation 
+            #apply debudd o blade 
+            #repeat cacualtions for next set of point/layer
+        #spead AFTER armour pen = caculatedForce - armourPen 
+     #if siccusful
+        #use points hit and spead AFTER armour pen to cacualte tissue dmg 
+        #apply debuff to effected party 
+        #stop other attack?
+            #if on limb delivering attack
+     return damage
+def announceRoundResults(damagePC, damageEn):
+#code to display the winner of the round goes here
+
+
+#there is nothing to return from this function
+
+
+def updateScore(winner, userScore, computerScore):
+    #code to u0pdate the score goes here
+
+
+    #this is the return statement that will send the values back
+    return userScore, computerScore
+
+
+
+def askForReplay(userName):
+    #code to ask if user wants to play again goes here
+
+
+    #this is the return statement that will send the value back
+return replayDesired
+
+
+def announceFinalResults(userName, userScore, computerScore):
+    #code to announce final results goes here
+
+
+
+    #there is nothing to return from this function
+
+
+
+
+def displayGameOver():
+#code to say game is over goes here
+#you could do that simply with printing to the console, but you might also want to do it with music, pictures, or maybe the turtle!
+
+
+#there is nothing to return from this function
+
